305af39e75c61627c0411720ecb7018d9e887e55
Tim Hannifan <hannifan@gmail.com>
2020-10-14 14:36:19 -0500

    completing ETL for mills data, added start-up process to retrieve mills data from opendata API, added local storage for mills data

--- web-server/app.py
+++ web-server/app.py
@@ -1,34 +1,38 @@
+# app.py
+#
+# Main application driver for Flask framework.
+#
+##
+
+
 import os
 import requests
 import json
 
 from flask import Flask, jsonify, render_template, abort, make_response
+from app_util import build_mills_records
+from log_util import logger
 
 MILLS_URL = "https://opendata.arcgis.com/datasets/5c026d553ff049a585b90c3b1d53d4f5_34.geojson"
+
 app = Flask(__name__)
+config = {'host': os.environ.get('APP_HOST', '0.0.0.0'), 
+          'port': os.environ.get("PORT", 5000)}
+mills_records = build_mills_records({},
+                                    MILLS_URL,
+                                    {'country': 'Indonesia'})
 
 
 @app.route("/api/v1.0/mills", methods=['GET'])
 def get_mills():
-    payload = {'country': 'Indonesia'}
-    req = requests.get(MILLS_URL, params=payload)
-    res_json = json.loads(req.text)
-
-    if 'features' not in res_json or len(res_json['features']) == 0:
-        abort(404)
-
-    mills = res_json['features']
-    mills_dict = {x["properties"]["objectid"] : x["properties"] for x in mills}
-    return jsonify({'mills': mills_dict})
+    return jsonify({'mills': mills_records})
 
 
 @app.route('/api/v1.0/mills/<int:mill_id>', methods=['GET'])
-def get_task(mill_id):
-    mills = []
-    mill = [mill for mill in mills if mill['id'] == mill_id]
-    if len(mill) == 0:
+def get_mill(mill_id):
+    if mill_id not in mills_records:
         abort(404)
-    return jsonify({'mill': mill[0]})
+    return jsonify({'mill': mills_records[mill_id]})
 
 
 @app.errorhandler(404)
@@ -37,4 +41,4 @@ def not_found(error):
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    app.run(host=config.get('host'), port=config.get('port'))
