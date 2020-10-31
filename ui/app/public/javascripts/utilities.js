15e9e166b2a247de32c2a6505b920f1b27cb6ef1
Launa Greer <launagreer@uchicago.edu>
2020-10-31 00:56:06 -0500

    Removes deprecated ui project

--- ui/app/public/javascripts/utilities.js
+++
@@ -1,5 +0,0 @@
-function formatNumber(num) {
-    console.log("hey")
-    let re = new RegExp('/(\d)(?=(\d{3})+(?!\d))/g', '$1,')
-    return num.toString().replace(re)
-}
--- ui/app/views/brand-stat.js
+++
@@ -1,6 +0,0 @@
-class AggregateBrandStatistics extends HTMLElement {
-    constructor() {
-        super();
-    }
-    
-}
