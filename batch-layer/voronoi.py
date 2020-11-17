2531607359ffe461f84d1c89ef349b2fb2474b88
Tim Hannifan <hannifan@gmail.com>
2020-11-16 20:25:57 -0600

    adding voronoi region, polygon, and point buffer calculation

--- batch-layer/voronoi.py
+++ batch-layer/voronoi.py
@@ -12,6 +12,8 @@ import pandas as pd
 from scipy.spatial import Voronoi
 from shapely.geometry import Point, Polygon
 
+BUFFER_SIZE = 5000
+BUFFER_RES = 12
 
 class Vor():
     def __init__(self, data=None):
@@ -34,10 +36,11 @@ class Vor():
         self.xy_gdf = self.latlon_gdf.to_crs("EPSG:3395")
         self.xy_pts = self.build_geom_list(self.xy_gdf)
         self._vor = Voronoi(self.xy_pts)
-        self.point_records = {}
-        self.pt_to_region_map = self.build_ptr_map()
 
-        # self.region_geometry = self.build_region_geometry()
+        self.point_map = self.build_ptr_map(self._vor)
+        self.region_map = self.build_region_map(self._vor)
+        self.polygon_map = self.build_polygon_map(self._vor)
+        self.buffered_points = self.build_point_buffer(self._vor)
 
     def build_geom_list(self, gdf):
         x_l = gdf.geometry.x.tolist()
@@ -45,15 +48,42 @@ class Vor():
         pts_l = [[x_l[i], y_l[i]] for i in range(len(x_l))]
         return np.array(pts_l)
 
-    def build_ptr_map(self):
-        vor = self._vor
+    def build_ptr_map(self, vor):
+        point_map = {}
         for i, pt_coords in enumerate(vor.points):
             region_assigned = vor.point_region[i]
 
-            self.point_records[i] = {
+            point_map[i] = {
+                'idx': i,
                 'coords': pt_coords,
-                'region_assigned': region_assigned
-            }
+                'region_assigned': region_assigned}
+        return point_map
 
-    # def build_region_geometry(self):
-        
+    def build_region_map(self, vor):
+        regions = {}
+
+        # vor.regions contains a lisst of vertices for each region.
+        # var.vertices contains a list of vertix coordinates.
+        for i, vertices in enumerate(vor.regions):
+            vtx_coords = [tuple(vor.vertices[v]) for v in vertices]
+            regions[i] = {'vertices': vertices, 'vtx_coords': vtx_coords}
+
+        return regions
+
+    def build_polygon_map(self, vor):
+        for k, v in self.region_map.items():
+            coords = v['vtx_coords']
+
+            polygon = Polygon()
+            # (A LinearRing must have at least 3 coordinate tuples)
+            if len(coords) >= 3:
+                polygon = Polygon(coords)
+            self.region_map[k]['polygon'] = polygon
+
+    def build_point_buffer(self, vor):
+        for k, point in self.point_map.items():
+            x = point['coords'][0]
+            y = point['coords'][1]
+
+            buff_point = Point(x, y).buffer(BUFFER_SIZE, resolution=BUFFER_RES)
+            self.point_map[k]['buffer'] = buff_point
