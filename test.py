4ba848e68aaafae7fad122872c995094f860c825
Amanda Whaley <>
2020-11-30 11:08:50 -0600

    Add 4 validation tests.

--- test.py
+++ test.py
@@ -1,37 +1,50 @@
 import unittest
 import requests
+import pandas as pd
 
 # Example From: https://docs.python.org/3/library/unittest.html
 
-class TestStringMethods(unittest.TestCase):
-
-    def test_upper(self):
-        self.assertEqual('foo'.upper(), 'FOO')
-
-    def test_isupper(self):
-        self.assertTrue('FOO'.isupper())
-        self.assertFalse('Foo'.isupper())
-
-    def test_split(self):
-        s = 'hello world'
-        self.assertEqual(s.split(), ['hello', 'world'])
-        # check that s.split fails when the separator is not a string
-        with self.assertRaises(TypeError):
-            s.split(2)
-
 class TestBrandInfo(unittest.TestCase):
+    # Get brands returns correct number of brands
+    def test_get_brands(self):
+        val_true = 8
+        test_url = 'http://0.0.0.0:5000/api/v1.0/brands'.format(id)
+        r = requests.get(test_url)
+        test_data = r.json()
+        val_test = len(test_data)
+        self.assertEqual(val_test, val_true)
+
     # Mean risk current
     def test_mean_risk_current(self):
-        data_test = 
-        val_test = requests. # Grab risk score current mean from webserver json
-        val_true = # Grab mean risk score current from uniquebrands.csv
+        id = 5
+        test_url = 'http://0.0.0.0:5000/api/v1.0/brands/{}'.format(id)
+        r = requests.get(test_url)
+        test_data = r.json()
+        val_test = test_data['avg_stats']['risk_score_current']['mean']
+        true_data = pd.read_csv('./data/bigtable.csv')
+        val_true = true_data[true_data['brandid']==id]['risk_score_current'].mean()
         self.assertEqual(val_test, val_true)
-    
+
     # Mill count
-    def test_mill_count(self):
-        pass
-    
-    # (Maybe) 404 error 
+    def test_brand_mill_count(self):
+        id = 3
+        test_url = 'http://0.0.0.0:5000/api/v1.0/brands/{}'.format(id)
+        r = requests.get(test_url)
+        test_data = r.json()
+        val_test = test_data['mill_count']
+        true_data = pd.read_csv('./data/bigtable.csv')
+        val_true = true_data[true_data['brandid']==id]['umlid'].count()
+        self.assertEqual(val_test, val_true)
+
+    # 404 error
+    def test_brand_404_error(self):
+        id = 25
+        test_url = 'http://0.0.0.0:5000/api/v1.0/brands/{}'.format(id)
+        r = requests.get(test_url)
+        val_test = r.status_code
+        val_true = 404
+        self.assertEqual(val_test, val_true)
+
 
 if __name__ == '__main__':
     unittest.main()
