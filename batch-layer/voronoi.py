9d750edb937f14f9f26735d6656f9bfc272dd590
Tim Hannifan <hannifan@gmail.com>
2020-12-15 14:45:02 -0600

    adding voronoi docstrings

--- batch-layer/voronoi.py
+++ batch-layer/voronoi.py
@@ -16,6 +16,7 @@ BUFFER_SIZE = 50000
 BUFFER_RES = 4
 
 class Vor():
+     """Generates Voronoi partitions from UML lat/lon coordinates."""
     def __init__(self, data=None):
         if data is not None:
             self.uml = data[['latitude', 'longitude']]
@@ -39,19 +40,20 @@ class Vor():
 
         self.point_map = self.build_ptr_map(self._vor)
         self.region_map = self.build_region_map(self._vor)
-        self.polygon_map = self.build_polygon_map(self._vor)
         self.buffered_points = self.build_point_buffer(self._vor)
 
         self.gen_intersections()
         self.output = self.gen_output()
 
     def build_geom_list(self, gdf):
+        """Converts geometries to np array."""
         x_l = gdf.geometry.x.tolist()
         y_l = gdf.geometry.y.tolist()
         pts_l = [[x_l[i], y_l[i]] for i in range(len(x_l))]
         return np.array(pts_l)
 
     def build_ptr_map(self, vor):
+        """Adds point coordinates and assigned regions to point dictionary."""
         point_map = {}
         for i, pt_coords in enumerate(vor.points):
             region_assigned = vor.point_region[i]
@@ -62,6 +64,7 @@ class Vor():
         return point_map
 
     def build_region_map(self, vor):
+        """Maps points to their polygon coordinates."""
         regions = {}
 
         # vor.regions contains a lisst of vertices for each region.
@@ -72,17 +75,8 @@ class Vor():
 
         return regions
 
-    def build_polygon_map(self, vor):
-        for k, v in self.region_map.items():
-            coords = v['vtx_coords']
-
-            polygon = Polygon()
-            # (A LinearRing must have at least 3 coordinate tuples)
-            if len(coords) > 2:
-                polygon = Polygon(coords)
-            self.region_map[k]['polygon'] = polygon
-
     def build_point_buffer(self, vor):
+        """Generates buffer surrounding x,y point."""
         for k, point in self.point_map.items():
             x = point['coords'][0]
             y = point['coords'][1]
@@ -91,8 +85,9 @@ class Vor():
             self.point_map[k]['buffer'] = buff_point
 
     def gen_intersections(self):
+        """Generates intersections of partitions and buffer."""
         for k, pt in self.point_map.items():
-            self.point_map[k]['boundary_coordss'] = None
+            self.point_map[k]['boundary_coords'] = None
             r_idx = pt['region_assigned']
             r_poly = self.region_map[r_idx]['polygon']
             b_poly = self.point_map[k]['buffer']
@@ -107,6 +102,7 @@ class Vor():
                 pass
 
     def gen_output(self):
+        """Main class driver, generates output file."""
         df = pd.DataFrame(self.point_map.values())
         df = df [['region_assigned', 'boundary_shape']]
 
@@ -119,8 +115,8 @@ class Vor():
                             geometry='boundary_shape',
                             crs={'init' :'epsg:3395'})
 
-        # TODO: missing geometries for 13 mills. See issues with intersections
-        # in shapely docs
+        # # Todo: Incomplete  geometries for mills. See issues with 
+        # # intersections in shapely docs
 
         # filter missing or empty geometries
         s = gdf['boundary_shape']
